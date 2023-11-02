661453317e174d3ea9457059a8b2529b03609536
mamoh00 <798184@stud.hvl.no>
2023-11-02 02:11:31 +0100

    added a table to the home page for mock data to have something to look at :)

--- client/src/pages/HomePage/index.tsx
+++ client/src/pages/HomePage/index.tsx
@@ -1,14 +1,70 @@
 // Home.tsx
 import React, { FC } from "react";
 import { Link } from "react-router-dom";
-import { Button, Typography } from "@mui/material";
+import { IconButton, Box, useTheme } from "@mui/material";
+import { DataGrid, GridCellParams } from "@mui/x-data-grid";
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import Navbar from "../../components/Navbar";
+import './index.css'
+
+const columns = [
+  { field: 'creator', headerName: 'Creator', width: 150 },
+  { field: 'title', headerName: 'Title', width: 150 },
+  { field: 'private', headerName: 'Private', width: 150 },
+  {
+    field: 'go to poll',
+    headerName: 'Go to Poll',
+    width: 150,
+    renderCell: (params: GridCellParams) => (
+      <IconButton
+        color="primary"
+        component={Link}
+        to={`/poll/${params.id}`} 
+      >
+        <ArrowForwardIcon color="primary"/>
+      </IconButton>
+    ),
+  },
+];
+
+const rows = [
+  { id: 1, creator: 'Frank Ove', title: 'Pizza eller Taco', private: 'No' },
+  { id: 2, creator: 'Odd Jostein', title: 'Ingen Lekse', private: 'Yes' },
+  { id: 3, creator: 'Per Gunnar', title: 'PS5 eller XBox', private: 'Yes' },
+  { id: 4, creator: 'Olav Hansen', title: 'LoL vs Dota', private: 'No' },
+  { id: 5, creator: 'Karoline Kvam', title: 'Erna vs Støre', private: 'No' },
+  { id: 6, creator: 'Henriette Lien', title: 'Windows eller Mac', private: 'Yes' },
+  { id: 7, creator: 'Sander Breivik', title: 'Android vs IPhone', private: 'No' },
+  { id: 8, creator: 'Jens Hjortdal', title: 'Billigere kantine?', private: 'Yes' },
+  { id: 9, creator: 'Karl Søreide', title: 'Mindre lekse', private: 'Yes' },
+  { id: 10, creator: 'Emilie Tanstad', title: 'Ny bybane?', private: 'No' },
+  // ... other rows
+];
+
 const Home: FC = () => {
+  const theme = useTheme();
   return (
-    <div>
+    <div style={{ margin: 0, padding: 0, overflowX: "hidden" }}>
       <Navbar />
-      
-      <h1>Home Page</h1>
+      <Box sx={{ height: 400, [theme.breakpoints.down("sm")]: {
+                  width: "100%",
+                },
+                width: '50%', p: 3, mx: 'auto' }}>  
+        <DataGrid
+          rows={rows}
+          columns={columns}
+          sx={{ border: '1px solid #CE8F6E', justifyContent: 'center' }}
+          className="customDataGrid"
+          initialState={{
+            pagination: {
+              paginationModel: {
+                pageSize: 5,
+              },
+            },
+          }}
+          pageSizeOptions={[5]}  // Adjust page size as needed
+        />
+      </Box>
     </div>
   );
 };
