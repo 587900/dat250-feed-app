ab4845e9caa571a41f57ac38f89cff83ce4012ae
KjetilBerg <kjetil.a.berg@hotmail.com>
2023-11-17 13:55:58 +0100

    move logout, make user-viewable-errors generic

--- server/lib/rest/router/auth.ts
+++ server/lib/rest/router/auth.ts
@@ -7,6 +7,7 @@ import IRouterClass from './../i-router-class';
 import StaticImplements from './../../decorator/static-implements';
 
 import Constants from './../../constants';
+import Logger from './../../logger';
 
 @StaticImplements<IRouterClass>()
 export default class AuthRouter {
@@ -14,11 +15,8 @@ export default class AuthRouter {
     public static create() : Router {
         let router = Router();
 
-        router.use('/check', (req, res) => {
-            let user = req.user;
-            if (user == null) return res.send({ authenticated: false });
-            return res.send({ authenticated: true, user });
-        });
+        router.use('/check', (req, res) => this.check(req, res));
+        router.get('/logout', (req, res) => this.logout(req, res));
 
         // Store redirect urls middleware
         router.use((_req, res, next) => {
@@ -27,31 +25,6 @@ export default class AuthRouter {
             this.cookie(res, Constants.SessionRedirectOnAuthFailure, req.bodyQuery[Constants.WebParamSessionRedirectOnAuthFailure]);
             next();
         });
-
-        router.get('/logout', (req, res) => {
-            req.logout((err: Error): void => {
-                if (err) {
-                    res.status(500).send('Error logging out');
-                    return;
-                }
-                
-                if (req.session) {
-                    req.session.destroy((err: Error): void => {
-                        if (err) {
-                            res.status(500).send('Failed to destroy session');
-                            return;
-                        }
-                        res.clearCookie('connect.sid');
-                        res.send({ success: true });
-                    });
-                } else {
-                    res.status(500).send('Session not found');
-                }
-            });
-        });
-        
-        
-        
 
         router.use('/google', this.basic('google'));
         router.use('/iot-device', this.crbasic('iot-device'));
@@ -62,6 +35,41 @@ export default class AuthRouter {
     private static cookie(res, key, value) {
         if (value == null) return;
         res.cookie(key, value);
+    }
+
+    private static check(req, res) {
+        let user = req.user;
+        if (user == null) return res.send({ authenticated: false });
+        return res.send({ authenticated: true, user });
+    }
+
+    private static logout(req, res) {
+        let user = req.user;
+        if (user == null) return res.send('not logged in');
+
+        req.logout((err : Error) => {
+            let logger = Logger.getLogger('/lib/rest/router/auth.ts');
+
+            if (err) {
+                logger.error(`User with id '${user.id}' failed to logout with error: ${err}`);
+                return res.sendStatus(500);
+            }
+
+            if (!req.session) {
+                logger.error(`User with id '${user.id}' did not have a session`);
+                return res.sendStatus(500);
+            }
+
+            req.session.destroy((err : Error) => {
+                if (err) {
+                    logger.error(`Failed to destroy session on user with id '${user.id}'`);
+                    return res.sendStatus(500);
+                }
+
+                res.clearCookie('connect.sid');
+                res.send({ success: true });
+            });
+        });
     }
 
     private static basic(name: string) : Router {
