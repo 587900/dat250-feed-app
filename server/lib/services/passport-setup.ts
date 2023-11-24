eb0705f11754fd06b0384a30f908318bace2f07f
KjetilBerg <kjetil.a.berg@hotmail.com>
2023-11-24 12:34:48 +0100

    change google-signin username field

--- server/lib/services/passport-setup.ts
+++ server/lib/services/passport-setup.ts
@@ -33,7 +33,7 @@ export default class PassportSetup {
             }, async (accessToken, refreshToken, profile, cb) => {
                 let user = await auth.getUserByGoogleId(profile.id);
                 if (user == null) {
-                    user = await auth.create(profile.name.givenName, profile.name.familyName, [ 'web-user' ], profile.email);
+                    user = await auth.create(profile.name.givenName, profile.name.familyName, [ 'web-user' ], profile.name.givenName + ' ' + profile.name.familyName);
                     if (user == null) return cb('failed to create account, username already exists', null);
                     let success = await auth.linkGoogleAccount(user.id, profile.id);
                     if (!success) {
