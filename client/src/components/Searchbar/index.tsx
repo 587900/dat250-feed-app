202db1d81e4744b58bd875a6141b6403ea89864b
mamoh00 <798184@stud.hvl.no>
2023-11-29 15:18:59 +0100

    added search functionality, still a bit buggy

--- client/src/components/Searchbar/index.tsx
+++ client/src/components/Searchbar/index.tsx
@@ -1,110 +1,57 @@
 import React, { useState, useEffect, FC } from "react";
 import Autocomplete from "@mui/material/Autocomplete";
 import TextField from "@mui/material/TextField";
-import ClearIcon from "@mui/icons-material/Clear";
 import SearchIcon from "@mui/icons-material/Search";
-import "@mui/material/styles";
-
-// Assuming OptionType is the type of options you are dealing with, replace it with your actual type
-interface OptionType {
-  name: string;
-  categoryId?: number; // Assuming categoryId is a number, adjust as per your data structure
-}
-
-const mockData = [
-  { name: "Apple", categoryId: 1 },
-  { name: "Banana", categoryId: 1 },
-  { name: "Cherry", categoryId: 1 },
-  { name: "Date", categoryId: 2 },
-  { name: "Elderberry", categoryId: 2 },
-  { name: "Fig", categoryId: 2 },
-  { name: "Grape", categoryId: 3 },
-  { name: "Honeydew", categoryId: 3 },
-  { name: "Indian Fig", categoryId: 3 },
-];
-
-// Assuming sortedOptions is a constant, replace it with your actual data
-const sortedOptions: OptionType[] = mockData;
+import { useNavigate } from 'react-router-dom';
+import { getFrontPagePolls } from "../../services/pollService";
+import APIPoll from "../../../../common/model/api-poll";
 
 const Searchbar: FC = () => {
-  const [recentSearches, setRecentSearches] = useState<OptionType[]>([]);
+  const [polls, setPolls] = useState<APIPoll[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const navigate = useNavigate();
 
-  // Function to handle selection from the autocomplete
-  const handleSelect = (
-    event: React.SyntheticEvent<Element, Event>,
-    value: string | OptionType,
-    reason: string,
-    details?: any
-  ) => {
-    if (typeof value !== "string" && !recentSearches.includes(value)) {
-      setRecentSearches([...recentSearches, value]);
+  useEffect(() => {
+    const fetchPolls = async () => {
+      const fetchedPolls = await getFrontPagePolls();
+      setPolls(fetchedPolls);
+    };
+
+    fetchPolls();
+  }, []);
+
+  const filteredOptions = polls.filter(poll => 
+    poll.title.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  const handleSelect = (event: React.SyntheticEvent, value: string | APIPoll) => {
+    if (typeof value === 'object' && value && 'code' in value) {
+      navigate(`/poll/vote/${value.code}`);
     }
-  };
-
-  // Function to remove a recent search
-  const removeRecentSearch = (item: OptionType) => {
-    setRecentSearches(recentSearches.filter((search) => search !== item));
-  };
+  };  
 
   return (
     <Autocomplete
       freeSolo
       disableClearable
-      open={isOpen}
-      onOpen={() => setIsOpen(true)}
-      onClose={() => setIsOpen(false)}
-      options={
-        inputValue
-          ? sortedOptions
-          : recentSearches.length
-          ? recentSearches
-          : sortedOptions
-      }
-      // The groupBy prop has been removed
-      getOptionLabel={(option) =>
-        typeof option === "string" ? option : option.name
-      }
+      options={filteredOptions}
+      getOptionLabel={(option) => typeof option === 'string' ? option : option.title || ''}
       inputValue={inputValue}
       onInputChange={(event, newInputValue) => {
         setInputValue(newInputValue);
       }}
       onChange={handleSelect}
-      renderOption={(props, option, { selected }) => (
-        <li {...props}>
-          {option.name}
-          {recentSearches.includes(option) && (
-            <ClearIcon
-              onClick={() => removeRecentSearch(option)}
-              sx={{ cursor: "pointer", marginLeft: "auto" }}
-            />
-          )}
-        </li>
-      )}
       renderInput={(params) => (
         <TextField
           {...params}
-          label="Search"
+          label="Search Polls"
           variant="outlined"
           placeholder="Type to search..."
-          InputLabelProps={{
-            style: { top: "-3px" },
-          }}
           InputProps={{
             ...params.InputProps,
-            style: { padding: "3px 8px" },
             endAdornment: (
               <>
-                {inputValue && (
-                  <ClearIcon
-                    onClick={() => {
-                      setInputValue("");
-                    }}
-                    sx={{ cursor: "pointer" }}
-                  />
-                )}
-                <SearchIcon sx={{ cursor: "pointer", color: 'primary.light' }} />
+                {inputValue && <SearchIcon sx={{ cursor: "pointer", color: 'primary.light' }} />}
                 {params.InputProps.endAdornment}
               </>
             ),
